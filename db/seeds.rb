8d0e116d03ce330ad4b342b301d96e4edc108408
Jose Anasco <joseanasco1@gmail.com>
2021-03-07 03:07:14 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Log.create ataque: true, tipo_ataque: 1, time_stamp_log: Time.zone.yesterday
+Log.create ataque: true, tipo_ataque: 2, time_stamp_log: Time.zone.yesterday
+Log.create ataque: true, tipo_ataque: 3, time_stamp_log: Time.zone.yesterday
+Log.create ataque: true, tipo_ataque: 4, time_stamp_log: Time.zone.yesterday
+
+Log.create ataque: false, tipo_ataque: 1, time_stamp_log: Time.zone.today.beginning_of_day + 1.hour
+Log.create ataque: false, tipo_ataque: 2, time_stamp_log: Time.zone.today.beginning_of_day + 2.hour
+Log.create ataque: false, tipo_ataque: 3, time_stamp_log: Time.zone.today.beginning_of_day + 3.hour
+Log.create ataque: false, tipo_ataque: 4, time_stamp_log: Time.zone.today.beginning_of_day + 4.hour
+
+Log.create ataque: true, tipo_ataque: 2, time_stamp_log: Time.zone.today.beginning_of_day + 1.hour
+Log.create ataque: true, tipo_ataque: 4, time_stamp_log: Time.zone.today.beginning_of_day + 1.hour
+
+Log.create ataque: true, tipo_ataque: 1, time_stamp_log: Time.zone.today.beginning_of_day + 2.hour
+Log.create ataque: true, tipo_ataque: 3, time_stamp_log: Time.zone.today.beginning_of_day + 2.hour
+Log.create ataque: true, tipo_ataque: 4, time_stamp_log: Time.zone.today.beginning_of_day + 2.hour
+
+Log.create ataque: true, tipo_ataque: 1, time_stamp_log: Time.zone.today.beginning_of_day + 3.hour
+Log.create ataque: true, tipo_ataque: 2, time_stamp_log: Time.zone.today.beginning_of_day + 3.hour
+
+Log.create ataque: true, tipo_ataque: 1, time_stamp_log: Time.zone.today.beginning_of_day + 4.hour
+Log.create ataque: true, tipo_ataque: 3, time_stamp_log: Time.zone.today.beginning_of_day + 4.hour
+Log.create ataque: true, tipo_ataque: 4, time_stamp_log: Time.zone.today.beginning_of_day + 4.hour
+
+Log.create ataque: true, tipo_ataque: 4, time_stamp_log: Time.zone.today.beginning_of_day + 5.hour
+Log.create ataque: true, tipo_ataque: 4, time_stamp_log: Time.zone.today.beginning_of_day + 5.hour
+Log.create ataque: true, tipo_ataque: 4, time_stamp_log: Time.zone.today.beginning_of_day + 5.hour
