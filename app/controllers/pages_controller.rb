2dcfe2feaac0cf73043e921a2a313524f32fb2c8
Jose Anasco <joseanasco1@gmail.com>
2021-03-15 03:22:28 +0100

    line chart graphic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,8 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_todays_logs
 
   def resumen_semanal
-    @today_logs =
-      Log.where("DATE(time_stamp_log) = ?", Time.zone.today)
-    @today_logs_with_attacks =
-      @today_logs.where(ataque: true)
   end
 
   def ataques
@@ -17,5 +14,14 @@ class PagesController < ApplicationController
   end
 
   def herramientas_de_analisis
+  end
+
+  private
+
+  def set_todays_logs
+    @today_logs =
+      Log.where("DATE(time_stamp_log) = ?", Time.zone.today)
+    @today_logs_with_attacks =
+      @today_logs.where(ataque: true)
   end
 end
