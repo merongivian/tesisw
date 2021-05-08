5093b3cfd9acc0a5bd88f2b41f68918c1598773c
Jose Añasco <joseanasco1@gmail.com>
2021-05-08 16:49:52 -0500

    action mailer setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,9 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: ENV['MAILER_PROD_HOST'] }
+  config.action_mailer.asset_host = ENV['MAILER_PROD_HOST']
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
 end
